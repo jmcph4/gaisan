60c8408fc8d735ff19a0d0f46a8191ff920ddb2e
jmcph4 <jmcph4@users.noreply.github.com>
2018-03-08 09:38:26 +1000

    Add basic matrix arithmetic
    This commit adds the four basic arithmetical operations on matrices:
    
        - Addition
        - Scaling (multiplication by a scalar)
        - Substraction
        - Multiplication (of two matrices)

--- src/matrix.c
+++ src/matrix.c
@@ -82,3 +82,121 @@ void matrix_sub_row(unsigned int a, unsigned int b, long double k,
     }
 }
 
+Matrix* matrix_add(Matrix* a, Matrix* b)
+{
+    if(a == NULL || b == NULL) /* null guard */
+    {
+        return NULL;
+    }
+
+    if(a->rows != b->rows || a->cols != b->cols) /* bounds check */
+    {
+        return NULL;
+    }
+
+    Matrix* res = matrix_init(a->rows, a->cols);
+
+    if(res != NULL) /* check for failure */
+    {
+        return NULL;
+    }
+
+    /* traverse both matrices, adding elementwise */
+    for(unsigned int i=0;i<a->rows;i++)
+    {
+        for(unsigned int j=0;j<a->cols;j++)
+        {
+            res->cells[i][j] = a->cells[i][j] + b->cells[i][j];
+        }
+    }
+
+    return res;
+}
+
+Matrix* matrix_scale(long double k, Matrix* matrix)
+{
+    if(matrix == NULL) /* null guard */
+    {
+        return NULL;
+    }
+
+    Matrix* res = matrix_init(matrix->rows, matrix->cols);
+
+    if(res == NULL) /* check for failure */
+    {
+        return NULL;
+    }
+
+    /* traverse matrix, multiplying by scalar k */
+    for(unsigned int i=0;i<matrix->rows;i++)
+    {
+        for(unsigned int j=0;j<matrix->cols;j++)
+        {
+            res->cells[i][j] = k * matrix->cells[i][j];
+        }
+    }
+
+    return res;
+}
+
+Matrix* matrix_subtract(Matrix* a, Matrix* b)
+{
+    if(a == NULL || b == NULL) /* null guard */
+    {
+        return NULL;
+    }
+
+    Matrix* negated_b = matrix_scale(-1, b);
+    
+    if(negated_b == NULL) /* check for failure */
+    {
+        return NULL;
+    }
+
+    Matrix* res = matrix_add(a, negated_b);
+
+    free(negated_b);
+
+    return res;
+}
+
+Matrix* matrix_multiply(Matrix* a, Matrix* b)
+{
+    if(a == NULL || b == NULL) /* null guard */
+    {
+        return NULL;
+    }
+
+    if(a->cols != b->rows) /* bounds check */
+    {
+        return NULL;
+    }
+
+    Matrix* res = matrix_init(a->rows, b->cols);
+
+    if(res == NULL) /* check for failure */
+    {
+        return NULL;
+    }
+
+    long double sum = 0.0; /* sum */
+
+    /* traverse matrices, performing naive matrix multiplication */
+    for(unsigned int i=0;i<a->rows;i++)
+    {
+        for(unsigned int j=0;j<b->cols;j++)
+        {
+            sum = 0.0;
+
+            for(unsigned int k=0;k<a->cols;k++)
+            {
+                sum += a->cells[i][k] * b->cells[k][j];
+            }
+
+            res->cells[i][j] = sum;
+        }
+    }
+
+    return res;
+}
+
--- src/matrix.h
+++ src/matrix.h
@@ -14,5 +14,11 @@ void matrix_free(Matrix* matrix);
 void matrix_sub_row(unsigned int a, unsigned int b, long double k,
         Matrix* matrix);
 
+Matrix* matrix_add(Matrix* a, Matrix* b);
+Matrix* matrix_scale(long double k, Matrix* matrix);
+Matrix* matrix_subtract(Matrix* a, Matrix* b);
+Matrix* matrix_multiply(Matrix* a, Matrix* b);
+
+
 #endif /* MATRIX_H_ */
 
