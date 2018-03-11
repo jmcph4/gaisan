a0c2de80fb02438ef2a19654182e73fc79dbd2a7
jmcph4 <jmcph4@users.noreply.github.com>
2018-03-11 13:59:45 +1000

    Add `LinSys` type for linear systems
    This commit adds the `LinSys` type for solving and manipulating
    linear systems of equations.

--- src/lin.c
+++ src/lin.c
@@ -1,1 +1,100 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "lin.h"
+
+LinSys* linsys_init(Matrix* A, Matrix* b)
+{
+    if(A == NULL || b == NULL)
+    {
+        return NULL;
+    }
+
+    if(A->rows != b->rows || b->cols != 1) /* bounds check */
+    {
+        return NULL;
+    }
+
+    LinSys* sys = calloc(1, sizeof(LinSys));
+
+    if(sys == NULL) /* allocation check */
+    {
+        return NULL;
+    }
+    
+    sys->A = matrix_copy(A);
+
+    if(sys->A == NULL) /* check for failure */
+    {
+        return NULL;
+    }
+
+    sys->b = matrix_copy(b);
+
+    if(sys->b == NULL) /* check for failure */
+    {
+        free(sys->A);
+        return NULL;
+    }
+
+    /* assign fields */
+    sys->A = A;
+    sys->b = b;
+    sys->x = NULL;
+
+    return sys;
+}
+
+void linsys_free(LinSys* linsys)
+{
+    if(linsys == NULL) /* null guard */
+    {
+        return;
+    }
+
+    free(linsys->A);
+    free(linsys->b);
+    free(linsys->x);
+    free(linsys);
+}
+
+void linsys_solve(LinSys* linsys)
+{
+    if(linsys == NULL)
+    {
+        return;
+    }
+
+    linsys->x = matrix_gauss_elim(linsys->A, linsys->b);
+}
+
+bool linsys_underdetermined(LinSys* linsys)
+{
+    if(linsys == NULL) /* null guard */
+    {
+        return false;
+    }
+
+    if(linsys->A->rows < linsys->A->cols)
+    {
+        return true;
+    }
+    
+    return false;
+}
+
+bool linsys_overdetermined(LinSys* linsys)
+{
+    if(linsys == NULL) /* null guard */
+    {
+        return false;
+    }
+
+    if(linsys->A->rows > linsys->A->cols)
+    {
+        return true;
+    }
+    
+    return false;
+}
 
--- src/lin.h
+++ src/lin.h
@@ -1,5 +1,24 @@
 #ifndef LIN_H_
-#define LIN_H_`
+#define LIN_H_
+
+#include <stdbool.h>
+
+#include "matrix.h"
+
+typedef struct
+{
+    Matrix* A;
+    Matrix* b;
+    Matrix* x;
+} LinSys;
+
+LinSys* linsys_init(Matrix* A, Matrix* b);
+void linsys_free(LinSys* linsys);
+
+void linsys_solve(LinSys* linsys);
+
+bool linsys_underdetermined(LinSys* linsys);
+bool linsys_overdetermined(LinSys* linsys);
 
 #endif /* LIN_H_ */
 
